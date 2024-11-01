7bf3eaa9db130e68ac24159b171dc20870322911
Anna Khit <annakhit02@mail.ru>
2024-11-01 20:26:37 +0300

    Implemented sorting application

---
+++ Speed.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public static class Speed
+{
+    static public double[] SpeedOfSortingInt(int length, params Func<int[], int[]>[] SortMethods)
+    {
+        double[] sum = new double[SortMethods.Length];
+        for (int i = 0; i < 20; i++)
+        {
+            int[] array = Generate.Int(length);
+            try
+            {
+                int[] sortedArray = null;
+                int index = 0;
+                foreach (Func<int[], int[]> Method in SortMethods)
+                {
+                    Stopwatch timer = new Stopwatch();
+                    timer.Start();
+                    sortedArray = Method((int[])array.Clone());
+                    timer.Stop();
+                    sum[index] += timer.ElapsedMilliseconds;
+                    index++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            };
+        }
+
+        return sum;
+    }
+
+    static public double[] SpeedOfSortingDouble(int length, params Func<double[], double[]>[] SortMethods)
+    {
+        double[] sum = new double[SortMethods.Length];
+        for (int i = 0; i < 20; i++)
+        {
+            double[] array = Generate.Double(length);
+            try
+            {
+                double[] sortedArray = null;
+                int index = 0;
+                foreach (Func<double[], double[]> Method in SortMethods)
+                {
+                    Stopwatch timer = new Stopwatch();
+                    timer.Start();
+                    sortedArray = Method((double[])array.Clone());
+                    timer.Stop();
+                    sum[index] += timer.ElapsedMilliseconds;
+                    index++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            };
+        }
+
+        return sum;
+    }
+
+    static public double[] SpeedOfSortingChar(int length, params Func<char[], char[]>[] SortMethods)
+    {
+        double[] sum = new double[SortMethods.Length];
+        for (int i = 0; i < 20; i++)
+        {
+            char[] array = Generate.Char(length);
+            try
+            {
+                char[] sortedArray = null;
+                int index = 0;
+                foreach (Func<char[], char[]> Method in SortMethods)
+                {
+                    Stopwatch timer = new Stopwatch();
+                    timer.Start();
+                    sortedArray = Method((char[])array.Clone());
+                    timer.Stop();
+                    sum[index] += timer.ElapsedMilliseconds;
+                    index++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            };
+        }
+
+        return sum;
+    }
+
+    static public double[] SpeedOfSortingString(int length, params Func<string[], string[]>[] SortMethods)
+    {
+        double[] sum = new double[SortMethods.Length];
+        for (int i = 0; i < 20; i++)
+        {
+            string[] array = Generate.String(length);
+            try
+            {
+                string[] sortedArray = null;
+                int index = 0;
+                foreach (Func<string[], string[]> Method in SortMethods)
+                {
+                    Stopwatch timer = new Stopwatch();
+                    timer.Start();
+                    sortedArray = Method((string[])array.Clone());
+                    timer.Stop();
+                    sum[index] += timer.ElapsedMilliseconds;
+                    index++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            };
+        }
+
+        return sum;
+    }
+}
---
+++ TreeNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TreeNode<T> where T : IComparable<T>
+{
+    public TreeNode(T data)
+    {
+        Data = data;
+    }
+
+    //данные
+    public T Data { get; set; }
+
+    //левая ветка дерева
+    public TreeNode<T> Left { get; set; }
+
+    //правая ветка дерева
+    public TreeNode<T> Right { get; set; }
+
+    //рекурсивное добавление узла в дерево
+    public void Insert(TreeNode<T> node)
+    {
+        if (Data.CompareTo(node.Data) > 0)
+        {
+            if (Left == null) Left = node;
+            else Left.Insert(node);
+        }
+        else
+        {
+            if (Right == null) Right = node;
+            else Right.Insert(node);
+        }
+    }
+
+    //преобразование дерева в отсортированный массив
+    public T[] Transform(List<T> elements = null)
+    {
+        if (elements == null) elements = new List<T>();
+
+        Left?.Transform(elements);
+
+        elements.Add(Data);
+
+        Right?.Transform(elements);
+
+        return elements.ToArray();
+    }
+}
